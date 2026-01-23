d54127dcbef730041c70cd8b4b9d911cba16732c
oz-codingschool-15nd <king11063064@gmail.com>
2026-01-23 17:02:34 +0900

    Delete 좋아요 CRUD directory

--- 좋아요 CRUD/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from apps.products import views
-
-urlpatterns = [
-    path("", views.ProductListCreateAPIView.as_view(), name="product-create-list"),
-    path("<int:product_id>/", views.ProductRetrieveUpdateDestroyAPIView.as_view(), name="product-detail"),
-    # feature/PostLikeAPI 전용 경로
-    path("<int:product_id>/like/", views.ProductLikeAPIView.as_view(), name="product-like"),
-]
